0bb131257e4def2bd1efebf9ce314dc8a186f2b3
Sachi King <nakato@nakato.io>
2026-02-06 14:58:54 +1100

    rt: 5.0.8 -> 6.0.2
    5.0.9 includeed security fixes.
    RT has had a major update, update to new RT major release.
    
    RT6 does not have a GD feature anymore, remove those dependencies.

--- pkgs/by-name/rt/rt/package.nix
+++ pkgs/by-name/rt/rt/package.nix
@@ -13,13 +13,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "rt";
-  version = "5.0.8";
+  version = "6.0.2";
 
   src = fetchFromGitHub {
     repo = "rt";
     rev = "rt-${finalAttrs.version}";
     owner = "bestpractical";
-    hash = "sha256-4/iC1PjLgLAp7XWTafe8HW3bTkDWWQxtSEIOs8wluzE=";
+    hash = "sha256-D5eTr7M24r2bMFkmC+MZVrlNs3zy8cgVvp9O3+P2weg=";
   };
 
   patches = [
@@ -43,6 +43,7 @@ stdenv.mkDerivation (finalAttrs: {
           BusinessHours
           CGIEmulatePSGI
           CGIPSGI
+          CSSInliner
           CSSMinifierXS
           CSSSquish
           ConvertColor
@@ -58,6 +59,7 @@ stdenv.mkDerivation (finalAttrs: {
           DateExtract
           DateManip
           DateTimeFormatNatural
+          DateTimeSet
           DevelGlobalDestruction
           EmailAddress
           EmailAddressList
@@ -67,10 +69,10 @@ stdenv.mkDerivation (finalAttrs: {
           FCGIProcManager
           FileShareDir
           FileWhich
-          GD
-          GDGraph
           GnuPGInterface
           GraphViz2
+          HashMerge
+          HashMergeExtra
           HTMLFormatExternal
           HTMLFormatTextWithLinks
           HTMLFormatTextWithLinksAndTables
@@ -80,6 +82,7 @@ stdenv.mkDerivation (finalAttrs: {
           HTMLQuoted
           HTMLRewriteAttributes
           HTMLScrubber
+          Imager
           IPCRun
           IPCRun3
           JSON
@@ -122,6 +125,7 @@ stdenv.mkDerivation (finalAttrs: {
           TextWikiFormat
           TextWordDiff
           TextWrapper
+          TimeLocal
           TimeParseDate
           TreeSimple
           UNIVERSALrequire
@@ -145,7 +149,6 @@ stdenv.mkDerivation (finalAttrs: {
   '';
   configureFlags = [
     "--enable-graphviz"
-    "--enable-gd"
     "--enable-gpg"
     "--enable-smime"
     "--with-db-type=Pg"
